7c983864e9bdcfcf9eeb567fa5d91c1a4fcfde41
Shaik Ikbhal Basha <iqbalforall@gmail.com>
2021-11-04 21:07:57 +0530

    tree demo only close, open

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<!-- https://svelte.dev/repl/a3f724e24df54b9695402576ffd68fe3?version=3.23.2 -->
+<script>
+	// from https://svelte.dev/repl/82b00644720a4ca2bdb89c6a653ec987
+	import TreeView from './TreeView.svelte'
+
+	const tree = {
+		label: "USA", children: [
+			{label: "Florida", children: [
+				{label: "Jacksonville"},
+				{label: "Orlando", children: [
+					{label: "Disney World"},
+					{label: "Universal Studio"},
+					{label: "Sea World"},
+				]},
+				{label: "Miami"},
+			]},
+			{label: "California", children: [
+				{label: "San Francisco"},
+				{label: "Los Angeles"},
+				{label: "Sacramento"},
+			]},
+		],
+	}
+</script>
+
+<TreeView {tree} />
---
+++ src/TreeView.svelte
@@ -0,0 +1,60 @@
+
+<!-- https://svelte.dev/docs#3_$_marks_a_statement_as_reactive -->
+<script context="module">
+	// retain module scoped expansion state for each tree node
+	const _expansionState = {
+		/* treeNodeId: expanded <boolean> */
+	}
+</script>
+
+<script>
+//	import { slide } from 'svelte/transition'
+	export let tree
+	const {label, children} = tree
+
+	let expanded = _expansionState[label] || false
+	const toggleExpansion = () => {
+		expanded = _expansionState[label] = !expanded
+	}
+    // https://svelte.dev/docs#3_$_marks_a_statement_as_reactive
+	$: arrowDown = expanded
+</script>
+
+<ul><!-- transition:slide -->
+	<li>
+		{#if children}
+			<span on:click={toggleExpansion}>
+                <!-- https://svelte.dev/tutorial/classes , https://svelte.dev/tutorial/class-shorthand-->
+				<span class="arrow" class:arrowDown>&#x25b6</span>
+				{label}
+			</span>
+			{#if expanded}
+				{#each children as child}
+					<svelte:self tree={child} />
+				{/each}
+			{/if}
+		{:else}
+			<span>
+				<span class="no-arrow"/>
+				{label}
+			</span>
+		{/if}
+	</li>
+</ul>
+
+<style>
+	ul {
+		margin: 0;
+		list-style: none;
+		padding-left: 1.2rem; 
+		user-select: none;
+        border-left: 1px dotted black;
+	}
+	.no-arrow { padding-left: 1.0rem; }
+	.arrow {
+		cursor: pointer;
+		display: inline-block;
+		/* transition: transform 200ms; */
+	}
+	.arrowDown { transform: rotate(90deg); }
+</style>
